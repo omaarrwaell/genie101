8c8e700729ce522fd02e0656616422514724737c
basilmarwan <basilmarwan2@gmail.com>
2024-09-24 14:41:39 +0200

    *

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:genie101/views/navigation_view.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-void main() => runApp(
-      DevicePreview(
-        builder: (context) => const MyApp(), // Wrap your app
-      ),
-    );
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,8 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       home: NavigationView(),
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
